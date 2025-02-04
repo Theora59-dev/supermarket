b49f27824f91481541b4aadfbf20d79ca6389d8c
theora59 <clem6kiruagon@gmx.fr>
2025-02-04 19:33:25 +0100

    commit pour la phase de test

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,51 @@
-use sdl3::*;
+extern crate sdl2;
 
-fn main() {
-    println!("Hello, world!");
+use sdl2::event::Event;
+use sdl2::gfx::primitives::DrawRenderer;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+
+pub fn main() {
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem
+        .window("rust-sdl2 demo", 800, 600)
+        .position_centered()
+        // .resizable()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().build().unwrap();
+
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
+    canvas.clear();
+    canvas.present();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    let mut i: i32 = 0;
+
+    'running: loop {
+        canvas.set_draw_color(Color::RGB(0, 0, 0));
+        canvas.clear();
+
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+                _ => {}
+            }
+        }
+
+        // The rest of the game loop goes here...
+        let _ = canvas
+            .filled_circle((400 + i / 100) as i16, 300, 100, Color::RGB(150, 0, 0))
+            .unwrap();
+        i += 1;
+
+        canvas.present();
+        // std::thread::sleep(std::time::Duration::new(0, 1_000_000_000u32 / 60));
+    }
 }
